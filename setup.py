03a6f4944daba25f380104fcdfe453fff5588d28
shinyshoes <shinyshoes404@protonmail.com>
2022-06-04 22:17:54 -0500

    finished setting up project root files

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme_long_description = fh.read()
+
+setup(
+    name='traffic-man',
+    version='0.0.1',
+    description="An application that checks for bad traffic between two points using the Google Maps API at set times and sends SMS notifications using Twilio.",
+    long_description=readme_long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/shinyshoes404/traffic-man',
+    author='shinyshoes',
+    author_email='shinyshoes404@protonmail.com',
+    license='MIT License',
+    packages=['traffic_man'],
+    package_dir={'':'src'},
+    #entry_points = { 'console_scripts' : ['example-command=package.module.function']},
+    
+    install_requires=[
+        'requests'
+    ],
+
+    extras_require={
+        # To install requirements for dev work use 'pip install -e .[dev]'
+        'dev': ['coverage', 'mock']
+    },
+
+    python_requires = '>=3.9.*',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: POSIX :: Linux',           
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+)
