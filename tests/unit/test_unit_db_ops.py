36bda5b0703308aa9790afe8809c47ef28e4668e
shinyshoes <shinyshoes404@protonmail.com>
2022-07-02 08:51:17 -0500

    unit tests done for db_ops

--- tests/unit/test_unit_db_ops.py
+++ tests/unit/test_unit_db_ops.py
@@ -1,5 +1,5 @@
 import unittest, mock, os, datetime
-from traffic_man.db_ops import DataSetup, TrafficDateTime, TrafficData
+from traffic_man.db_ops import DataSetup, TrafficDateTime, TrafficData, SMSData
 
 class TestDataSetup(unittest.TestCase):
 
@@ -574,9 +574,217 @@ class TestTrafficData(unittest.TestCase):
                 test_obj = TrafficData(mock_engine)
                 check_val = test_obj.check_traffic_conditions()
                 self.assertEqual(check_val, "traffic")
+
     
+    ### --------------------- TrafficData.write_bad_traffic() ----------------------
+    def test_write_bad_traffic_except_on_execute(self):
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(side_effect=Exception("execute exception"))
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.traffic_conditions.insert") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.write_bad_traffic()
+                self.assertIs(check_val, None)
+
+    def test_write_bad_traffic_success(self):
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(return_value=mock.Mock())
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.traffic_conditions.insert") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.write_bad_traffic()
+                self.assertIs(check_val, True)
+
+    ### --------------------- TrafficData.write_traffic_resolved() ----------------------
+    def test_write_traffic_resolved_except_on_execute(self):
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(side_effect=Exception("execute exception"))
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.traffic_conditions.update") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.write_traffic_resolved()
+                self.assertIs(check_val, None)
+
+    def test_write_traffic_resolved_success(self):
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(return_value=mock.Mock())
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.traffic_conditions.update") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.write_traffic_resolved()
+                self.assertIs(check_val, True)
 
 
-# test_obj = TestTrafficData()
-# test_obj.test_check_traffic_conditions_no_traffic_conditions()
-# test_obj.test_update_check_times_except_on_delete()
+    ### --------------------- TrafficData.get_phone_numbers() ----------------------
+    def test_get_phone_numbers_except_on_execute(self):
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(side_effect=Exception("execute exception"))
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.db.select") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.get_phone_numbers()
+                self.assertIs(check_val, None)
+
+    def test_get_phone_numbers_two_phone_nums(self):
+        # define a mock results object
+        mock_results = mock.Mock()
+        mock_results.fetchall.return_value = [('+15555555555',),('+13333333333',)]
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(return_value=mock_results)
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.db.select") as mock_select:
+                test_obj = TrafficData(mock_engine)
+                check_val = test_obj.get_phone_numbers()
+                self.assertEqual(len(check_val), 2)
+                self.assertEqual(check_val[1], '+13333333333')
+
+
+class TestSMSData(unittest.TestCase):
+
+    ### --------------------- SMSData.check_sms_today() ----------------------
+    def test_check_sms_today_except_on_execute(self):
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(side_effect=Exception('execute exception'))
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.sms_data.select") as mock_select:
+                test_obj = SMSData(mock_engine)
+                check_val = test_obj.check_sms_today("traffic")
+                self.assertIs(check_val, None)
+
+    def test_check_sms_today_none_sent_yet(self):
+        # define a mock results object
+        mock_results = mock.Mock()
+        mock_results.fetchall.return_value = []
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(return_value=mock_results)
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.sms_data.select") as mock_select:
+                test_obj = SMSData(mock_engine)
+                check_val = test_obj.check_sms_today("traffic")
+                self.assertIs(check_val, False)
+
+    def test_check_sms_today_msg_sent_today(self):
+        # define a mock results object
+        mock_results = mock.Mock()
+        mock_results.fetchall.return_value = [(1, '2022-10-22', 'traffic', 0)]
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(return_value=mock_results)
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.sms_data.select") as mock_select:
+                test_obj = SMSData(mock_engine)
+                check_val = test_obj.check_sms_today("traffic")
+                self.assertIs(check_val, True)
+
+    ### --------------------- SMSData.write_sms_record() ----------------------
+    def test_write_sms_record_except_on_execute(self):
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(side_effect=Exception('execute exception'))
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.sms_data.insert") as mock_select:
+                test_obj = SMSData(mock_engine)
+                check_val = test_obj.write_sms_record("traffic", 0)
+                self.assertIs(check_val, None)
+
+    def test_write_sms_record_success(self):
+
+        # define a mock connection object
+        mock_conn = mock.Mock()
+        mock_conn.execute = mock.PropertyMock(mock.Mock())
+
+        # define a mock engine object
+        mock_engine = mock.Mock()         
+        mock_engine.connect = mock.PropertyMock(new_callable=mock.Mock())
+        mock_engine.connect.return_value.__enter__.return_value = mock_conn # need .return_value.__enter__.return_value for the with as statement in our app
+
+        with mock.patch("traffic_man.db_ops.datetime") as mock_dt:
+            mock_dt.now.return_value = datetime.datetime(2022, 10, 22, 0, 0) # sets self.curr_date in __init__
+            with mock.patch("traffic_man.db_ops.sms_data.insert") as mock_select:
+                test_obj = SMSData(mock_engine)
+                check_val = test_obj.write_sms_record("traffic", 0)
+                self.assertIs(check_val, True)
+
