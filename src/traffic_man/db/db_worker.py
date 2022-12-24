12bd6384327b7d859767312c0110ca1c373e3127
shinyshoes <shinyshoes404@protonmail.com>
2022-12-24 11:38:40 -0600

    finished tests for db_worker

--- src/traffic_man/db/db_worker.py
+++ src/traffic_man/db/db_worker.py
@@ -47,6 +47,3 @@ def db_worker(kill_q, db_req_q, db_res_traffic_eng_q):
                 db_res_traffic_eng_q.put({"msg-id": msg_id, "msg_src": msg_src, "results": db_op_result})
         except Empty:
             logger.debug("db request queue is empty")
-
-
-     
