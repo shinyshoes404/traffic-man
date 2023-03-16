ceba52ea915ffbbabfb2f533728279f28310cf97
shinyshoes <shinyshoes404@protonmail.com>
2023-03-15 21:27:20 -0500

    moved modified TwilioSignature class into test_script.py

--- tests/End2End/test_script.py
+++ tests/End2End/test_script.py
@@ -1,5 +1,31 @@
-from traffic_man.twilio.twilio import TwilioSignature
-import requests, sys, sqlite3
+import os, base64, requests, hashlib, hmac, sys, sqlite3
+
+
+class TwilioSignature:
+    def __init__(self, request_body, headers: dict):
+        self.request_body = request_body
+        self.headers = headers
+   
+    def _create_param_str(self) -> str:
+        req_body_dict = self.request_body.form.to_dict()
+        keys = list(req_body_dict.keys())
+        keys.sort()
+        param_str = ""
+        for key in keys:
+            param_str = param_str + key + req_body_dict.get(key)
+        
+        return param_str
+    
+    def _create_signature(self) -> str:
+        key = bytes(os.environ.get("TWILIO_AUTH_TOKEN"), "UTF-8")
+        contents = bytes(os.environ.get("TWILIO_WEBHOOK_URL") + self._create_param_str(), "UTF-8")
+        hmac_obj = hmac.new(key, contents, hashlib.sha1)
+        signature = hmac_obj.digest()
+        # encode hmac signature to base64, then decode bytes to be a utf-8 string
+        signature_base64_str = base64.b64encode(signature).decode('UTF-8')
+
+        return signature_base64_str
+
 
 
 # classes to reuse TwilioSignature to create test request signatures
